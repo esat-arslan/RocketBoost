5d5ef488cadb1262d457c70c2af0cf454211c081
Esat Arslan <esat.arslan.dev@gmail.com>
2025-12-22 20:08:37 +0300

    Added logic for reloading level and next level

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,53 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Looking good!");
+                break;
+            case "Finish":
+                LoadNextLevel();
+                break;
+            case "Fuel":
+                Debug.Log("Whoop Whoop!");
+                break;
+            case "Ground":
+                StartCrashSequence();
+                break;
+            default:
+                Debug.Log("Booooooom!");
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartCrashSequence()
     {
+        Invoke("ReloadLevel", 2f); 
+    }
+
+    private static void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currenSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currenSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
         
     }
 }
