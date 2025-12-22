05043561284b3ad0779565feaa215e5303a6d90c
Esat Arslan <esat.arslan.dev@gmail.com>
2025-12-22 20:20:03 +0300

    Implemented a better load next level and reload current level logic

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,23 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CollisionHandller : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 2f;
+    bool isTransitioning = false;
     private void OnCollisionEnter(Collision other)
     {
+        if (isTransitioning) {return;}
+
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("Looking good!");
                 break;
             case "Finish":
-                LoadNextLevel();
+                StartSuccessSequence();
                 break;
             case "Fuel":
                 Debug.Log("Whoop Whoop!");
@@ -28,10 +33,33 @@ public class CollisionHandller : MonoBehaviour
 
     private void StartCrashSequence()
     {
-        Invoke("ReloadLevel", 2f); 
+        GetComponent<RocketMovement>().enabled = false;
+        isTransitioning = true;
+        StartCoroutine(ReloadLevelAfterDelay());
     }
 
-    private static void ReloadLevel()
+    private IEnumerator ReloadLevelAfterDelay()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
+        int currenSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currenSceneIndex);
+    }
+
+    private void StartSuccessSequence()
+    {
+        isTransitioning = false;
+        StartCoroutine(LoadNextLevelAfterDelay());
+    }
+
+    private IEnumerator LoadNextLevelAfterDelay()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
+        int currenSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currenSceneIndex + 1 ) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
@@ -48,6 +76,6 @@ public class CollisionHandller : MonoBehaviour
         }
 
         SceneManager.LoadScene(nextSceneIndex);
-        
+
     }
 }
